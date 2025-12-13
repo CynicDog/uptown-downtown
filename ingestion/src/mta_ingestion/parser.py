05b089d4aac70f46f62a84c2b4228908e522d97e
eunsang <thecynicdog0328@gmail.com>
2025-12-13 13:28:10 +0900

    Fix parse_feed to correctly read NYCT GTFS-Realtime extensions
    - Access base TripUpdate and StopTimeUpdate from gtfs_realtime_pb2
    - Extract NYCT-specific fields via nyct_trip_descriptor and nyct_stop_time_update
    - Include arrival, departure times and track info in ingestion rows

--- ingestion/src/mta_ingestion/parser.py
+++ ingestion/src/mta_ingestion/parser.py
@@ -1,5 +1,4 @@
 import datetime
-
 from mta_ingestion.proto import gtfs_realtime_NYCT_pb2 as nyct_pb2
 
 
@@ -10,47 +9,40 @@ def parse_feed(feed, ingestion_ts: datetime.datetime) -> list[dict]:
         if not entity.HasField("trip_update"):
             continue
 
-        trip_update = entity.trip_update
+        trip_update = entity.trip_update  # from base GTFS
         trip = trip_update.trip
-
         nyct_trip = trip.Extensions[nyct_pb2.nyct_trip_descriptor]
 
         for stu in trip_update.stop_time_update:
-            nyct_stop = stu.Extensions[
-                nyct_pb2.nyct_stop_time_update
-            ]
+            nyct_stop = stu.Extensions[nyct_pb2.nyct_stop_time_update]
 
-            arrival_time = None
-            if (
-                stu.HasField("arrival")
-                and stu.arrival.HasField("time")
-            ):
-                arrival_time = datetime.datetime.fromtimestamp(
-                    stu.arrival.time
-                )
-
-            rows.append(
-                {
-                    "ingestion_ts": ingestion_ts,
-                    "trip_id": trip.trip_id,
-                    "route_id": trip.route_id,
-                    "direction": (
-                        nyct_pb2.NyctTripDescriptor.Direction.Name(
-                            nyct_trip.direction
-                        )
-                        if nyct_trip.HasField("direction")
-                        else None
-                    ),
-                    "stop_id": stu.stop_id,
-                    "arrival_time": arrival_time,
-                    "scheduled_track": (
-                        nyct_stop.scheduled_track or None
-                    ),
-                    "actual_track": (
-                        nyct_stop.actual_track or None
-                    ),
-                    "is_assigned": nyct_trip.is_assigned,
-                }
+            arrival_time = (
+                datetime.datetime.fromtimestamp(stu.arrival.time)
+                if stu.HasField("arrival") and stu.arrival.HasField("time")
+                else None
             )
+            departure_time = (
+                datetime.datetime.fromtimestamp(stu.departure.time)
+                if stu.HasField("departure") and stu.departure.HasField("time")
+                else None
+            )
+
+            rows.append({
+                "ingestion_ts": ingestion_ts,
+                "trip_id": trip.trip_id,
+                "route_id": trip.route_id,
+                "train_id": nyct_trip.train_id if nyct_trip.HasField("train_id") else None,
+                "direction": (
+                    nyct_pb2.NyctTripDescriptor.Direction.Name(nyct_trip.direction)
+                    if nyct_trip.HasField("direction")
+                    else None
+                ),
+                "is_assigned": nyct_trip.is_assigned,
+                "stop_id": stu.stop_id,
+                "arrival_time": arrival_time,
+                "departure_time": departure_time,
+                "scheduled_track": nyct_stop.scheduled_track or None,
+                "actual_track": nyct_stop.actual_track or None,
+            })
 
     return rows
